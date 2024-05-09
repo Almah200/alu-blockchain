e6b7cc84d9c224e09b5284c9ce21958237c7682d
almarat <arnualmarat@gmail.com>
2024-05-09 10:09:40 +0300

    all

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,25 +2,30 @@
 #include <stddef.h>
 #include <openssl/sha.h>
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]) {
-    SHA256_CTX ctx;
+uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+{
+SHA256_CTX ctx;
 
     /* Check if digest is NULL */
-    if (digest == NULL) {
-        return NULL;
-    }
+if (digest == NULL)
+{
+return (NULL);
+}
 
     /* Compute SHA-256 hash */
-    if (!SHA256_Init(&ctx)) {
-        return NULL;
-    }
-    if (!SHA256_Update(&ctx, s, len)) {
-        return NULL;
-    }
-    if (!SHA256_Final(digest, &ctx)) {
-        return NULL;
-    }
-    
+if (!SHA256_Init(&ctx))
+{
+return (NULL);
+}
+if (!SHA256_Update(&ctx, s, len))
+{
+return (NULL);
+}
+if (!SHA256_Final(digest, &ctx))
+{
+return (NULL);
+}
+
     /* Return pointer to digest */
-    return digest;
+return (digest);
 }
