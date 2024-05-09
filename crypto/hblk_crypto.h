70834347d64c769d62dde9d39b26d4c436004731
almarat <arnualmarat@gmail.com>
2024-05-09 10:17:35 +0300

    all

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -14,6 +14,7 @@
  *
  * Return: Pointer to digest if digest is not NULL, otherwise NULL
  */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s, size_t len,
+uint8_t digest[SHA256_DIGEST_LENGTH]);
 
 #endif /* HBLK_CRYPTO_H */
--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,7 +2,19 @@
 #include <stddef.h>
 #include <openssl/sha.h>
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+/*
+ * Computes the SHA-256 hash of a given input string.
+ *
+ * Parameters:
+ *   s: Input string to compute hash for.
+ *   len: Length of the input string.
+ *   digest: Array to store the resulting hash digest.
+ *
+ * Returns:
+ *   Pointer to the resulting hash digest.
+ */
+uint8_t *sha256(int8_t const *s, size_t len,
+uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 SHA256_CTX ctx;
 
